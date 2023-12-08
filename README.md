7f8a5b8a0bf0da9d175a2f28eb82c78f00fd5dbd
qinjie <mark.qj@gmail.com>
2023-12-08 12:01:22 +0800

    Add function to override service limits

--- README.md
+++ README.md
@@ -4,10 +4,24 @@ A sample script to use [awslimitchecker](https://github.com/jantman/awslimitchec
 
 ### Note
 
-1. Include CloudTrail service will cause an error during execution. Need to exclude it in the script.
+This library is slightly outdated. It should be
+
+1. Including CloudTrail service will cause an error during execution. Need to exclude it in the script.
 
 ```
 c.remove_services(['CloudTrail'])
+```
+
+2. The actual CloudFront quotas are not available in the Service Quota SDK. Thus we need to submit an AWS Support case to find out the actual applied quota.
+
+```
+c.set_limit_override('CloudFront', '<QUOTA_NAME>', <QUOTA_VALUE>)
+```
+
+3. The limit `EBS: General Purpose (SSD gp3) volume storage (GiB)` should be `EBS: Storage for General Purpose SSD (gp3) volumes, in TiB`. Find the applied quota from https://ap-southeast-1.console.aws.amazon.com/servicequotas/home/services/ebs/quotas/L-7A658B76 (Update region value accordingly), and add following statements.
+
+```
+c.set_limit_override('EBS', 'General Purpose (SSD gp3) volume storage (GiB)', <QUOTA_VALUE>)
 ```
 
 ### Sample Output
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from typing import List,Tuple
+from awslimitchecker.checker import AwsLimitChecker
+from typing import List, Tuple
 import logging
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
 
-from awslimitchecker.checker import AwsLimitChecker
 
 regions = ['ap-southeast-1', 'ap-northeast-1']
 
@@ -12,21 +12,24 @@ regions = ['ap-southeast-1', 'ap-northeast-1']
 WARNING_THRESHOLD = 70
 CRITICAL_THRESHOLD = 85
 
-# Set custom threshold for some limits of some services
+# Set custom thresholds for some limits of some services
 service_limit_thresholds = [
-        dict(service_name='VPC', limit_name='NAT Gateways per AZ', warn_percent=None, warn_count=2, crit_percent=None, crit_count=3) 
-    ]
+    # dict(service_name='VPC', limit_name='NAT Gateways per AZ', warn_percent=None, warn_count=2, crit_percent=None, crit_count=3),
+]
 
-def check_limit_by_region(region: str)-> Tuple[List[str],List[str]]:
+# Override default limit values with applied limits: [service_name, limit_name, override_value],
+service_limit_overrided = [
+    ['EBS', 'General Purpose (SSD gp3) volume storage (GiB)', 50*1024],
+    ['CloudFront', 'Cache behaviors per distribution', 25]
+]
+
+
+def get_checker(region: str) -> AwsLimitChecker:
     """
-    Find the list of services which has reached warning or critical usage limit.
-    Arguments:
-        region: aws region code
-    Returns:
-        A tuple contains warning list and critical list, where each item in the list is a comma-delimited value "service,limit_name,usage,limit".
+    Return an initialized checker with custom and override values applied.
     """
-    c = AwsLimitChecker(region=region, 
-                        warning_threshold=WARNING_THRESHOLD, 
+    c = AwsLimitChecker(region=region,
+                        warning_threshold=WARNING_THRESHOLD,
                         critical_threshold=CRITICAL_THRESHOLD)
 
     # print(c.services)
@@ -36,8 +39,25 @@ def check_limit_by_region(region: str)-> Tuple[List[str],List[str]]:
     for thresholds in service_limit_thresholds:
         c.set_threshold_override(**thresholds)
 
+    # Override default limit values with applied limits
+    for override_vals in service_limit_overrided:
+        c.set_limit_override(*override_vals)
+
     # Skip CloudTrail because it will cause an error
     c.remove_services(['CloudTrail'])
+
+    return c
+
+
+def check_limit_by_region(region: str) -> Tuple[List[str], List[str]]:
+    """
+    Find the list of services which has reached warning or critical usage limit.
+    Arguments:
+        region: aws region code
+    Returns:
+        A tuple contains warning list and critical list, where each item in the list is a comma-delimited value "service,limit_name,usage,limit".
+    """
+    c = get_checker(region)
 
     warnings = []
     criticals = []
@@ -45,27 +65,29 @@ def check_limit_by_region(region: str)-> Tuple[List[str],List[str]]:
     for service, svc_limits in result.items():
         for limit_name, limit in svc_limits.items():
             for warn in limit.get_warnings():
-                warnings.append(f'{service},{limit_name},{str(warn)},{limit.get_limit()}')
+                warnings.append(
+                    f'{service},{limit_name},{str(warn)},{limit.get_limit()}')
 
             for crit in limit.get_criticals():
-                criticals.append(f'{service},{limit_name},{str(crit)},{limit.get_limit()}')
+                criticals.append(
+                    f'{service},{limit_name},{str(crit)},{limit.get_limit()}')
 
     return warnings, criticals
 
 
 if __name__ == '__main__':
     for region in regions:
-        warnings,criticals = check_limit_by_region(region)
+        warnings, criticals = check_limit_by_region(region)
         if warnings:
             print(f'\nWarning: {region}')
             for item in warnings:
-                if len(item.split(','))==4:
-                    service,limit_name,usage,limit = item.split(',')
+                if len(item.split(',')) == 4:
+                    service, limit_name, usage, limit = item.split(',')
                     print(f'\t{service}: {limit_name} {usage} <= {limit}')
 
         if criticals:
             print(f'\nCritical: {region}')
             for item in criticals:
-                if len(item.split(','))==4:
-                    service,limit_name,usage,limit = item.split(',')
+                if len(item.split(',')) == 4:
+                    service, limit_name, usage, limit = item.split(',')
                     print(f'\t{service}: {limit_name} {usage} <= {limit}')
